d7a5dcd202efbd163475c2b79d781da114d0b97f
da1sy petals <11695641+da1sypetals@user.noreply.gitee.com>
2024-11-29 12:38:24 +0800

    versioning

--- src/symars/__init__.py
+++ src/symars/__init__.py
@@ -1,5 +1,5 @@
-from .faer import SymarsFaer
+from .dense_impl.faer import SymarsFaer
 from .dense import SymarsDense
 from .uni import SymarsUni
-from .nalgebra import SymarsNalgebra
+from .dense_impl.nalgebra import SymarsNalgebra
 from .meta import DType
