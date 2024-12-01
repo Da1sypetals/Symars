ae1a3b1ff35ab75c1188bfe036323a17f600bc42
daisy petals <da1sypetals.iota@gmail.com>
2024-12-01 11:26:40 +0800

    rest repo

--- rust_generated/src/main.rs
+++
@@ -1,6 +0,0 @@
-use symars_demonstration::sym::gen_mat_nalgebra;
-
-fn main() {
-    let mat = gen_mat_nalgebra::test_matrix(1.0, 1.0, 5.0, 1.0, 1.0, 1.0, 1.0, 1.0);
-    dbg!(mat);
-}
--- rust_generated/src/sym/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod gen_mat_array;
-pub mod gen_mat_faer;
-pub mod gen_mat_nalgebra;
-pub mod gen_sparse;
-pub mod gen_uni;
-pub mod gen_vec_faer;
